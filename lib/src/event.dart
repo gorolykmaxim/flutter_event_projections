f54237f3503e98a07b890d9fd174c57e49c40e93
gorolykmaxim <streetbattle52@gmail.com>
2019-11-26 13:01:32 +0200

    Allow aggregation of multiple events into one
    Create ObservableEventStream, which allows listening to events,
    that are being published on it (it inherits EventStream, so it
    can be passed in places where EventStream is used).
    Also allows aggregating specified kinds of events into new events
    and listening to them.

--- lib/src/event.dart
+++ lib/src/event.dart
@@ -79,3 +79,138 @@ class EventStream<T> {
     _controller.addError(exception);
   }
 }
+
+/// Mapping between names of entities, that are present in events, being
+/// aggregated, and their names in the resulting aggregating event.
+class EntityMapping {
+  final Map<String, Map<String, String>> _eventTypeToOriginalEntityToTargetEntity = {};
+
+  /// Specify that while aggregating event with [eventType], ID of
+  /// [originalEntity] from the source event should be transferred to
+  /// [targetEntity] in the aggregating event.
+  void set(String eventType, String originalEntity, String targetEntity) {
+    _eventTypeToOriginalEntityToTargetEntity.putIfAbsent(eventType, () => {});
+    _eventTypeToOriginalEntityToTargetEntity[eventType][originalEntity] = targetEntity;
+  }
+
+  /// Get a name, by which ID of [originalEntity] in event of [eventType]
+  /// should be stored in the aggregating event.
+  String get(String eventType, String originalEntity) {
+    final originalEntityToTargetEntity = _eventTypeToOriginalEntityToTargetEntity[eventType];
+    if (originalEntityToTargetEntity != null) {
+      return originalEntityToTargetEntity[originalEntity] ?? originalEntity;
+    } else {
+      return originalEntity;
+    }
+  }
+}
+
+/// Strategy of creating event aggregations.
+///
+/// Life cycle of strategy instance is directly bound to a life cycle of a
+/// [ObservableEventStream], on which the strategy continuously aggregates
+/// events. This means, that if a strategy has a state, it should properly
+/// manage it while creating different aggregations.
+abstract class AggregationStrategy<T> {
+  /// Try to create an aggregation of events when [event] happens.
+  ///
+  /// If [event] finishes the process of aggregation of multiple events,
+  /// that were previously passed to this strategy, the strategy should emit
+  /// an event that represents an aggregation of all previously happened events
+  /// that should belong to it.
+  ///
+  /// If [event] does not finish any aggregation, strategy should return null.
+  Event<T> tryToAggregateOn(Event<T> event);
+
+  /// Create an event with [newEventName], that is an aggregation of all
+  /// [events].
+  ///
+  /// Each name of each entity, specified in the aggregating event, is
+  /// determined using [mapping].
+  Event<T> aggregate(Iterable<Event<T>> events, String newEventName, EntityMapping mapping) {
+    final entityToId = <String, T>{};
+    for (var event in events) {
+      final entitiesOfEvent = event.toMap();
+      for (var entity in entitiesOfEvent.keys) {
+        entityToId[mapping.get(event.name, entity)] = entitiesOfEvent[entity];
+      }
+    }
+    return Event(newEventName, entityToId);
+  }
+}
+
+/// The most basic [AggregationStrategy], that assumes that all aggregations
+/// happen sequentially: if there are event types A and B, that should form
+/// an aggregation C, than only the following flow of events is possible:
+/// A, B, A, B, B, A, A, B
+///
+/// This strategy will not work in cases when there can be multiple aggregations
+/// happening at the same time:
+/// A, A, B, B, A, A, A, B, B, B
+class Sequential<T> extends AggregationStrategy<T> {
+  final Set<String> _expectedEventTypes;
+  final String _newEventName;
+  final EntityMapping _mapping;
+  final Set<Event<T>> _occurredEvents = Set();
+  final int _timeout;
+  int _lastMatchingEventOccurrenceTime = 0;
+
+  /// Create strategy which creates aggregation of [_expectedEventTypes].
+  ///
+  /// Use [_factory] to create an event, that represents an aggregation of
+  /// occurred events.
+  ///
+  /// If event, that belongs to an aggregation, that is currently being
+  /// constructed, does not happen for a [timeout], than the current aggregation
+  /// will be discarded and a new one will be built instead.
+  ///
+  /// If [timeout] is set to 0 - incomplete aggregations will not get discarded
+  /// over time.
+  Sequential(Iterable<String> expectedEventTypes, this._newEventName, {EntityMapping mapping, int timeout = 0}):
+        _expectedEventTypes = Set.from(expectedEventTypes),
+        _mapping = mapping ?? EntityMapping(),
+        _timeout = timeout;
+
+  Event<T> tryToAggregateOn(Event<T> event) {
+    int currentTimeStamp = DateTime.now().millisecondsSinceEpoch;
+    if (_timeout > 0 && currentTimeStamp > _lastMatchingEventOccurrenceTime + _timeout) {
+      _occurredEvents.clear();
+    }
+    if (_expectedEventTypes.contains(event.name)) {
+      _occurredEvents.add(event);
+      _lastMatchingEventOccurrenceTime = currentTimeStamp;
+    }
+    final occurredEventTypes = _occurredEvents.map((e) => e.name).toSet();
+    if (occurredEventTypes.containsAll(_expectedEventTypes)) {
+      final aggregation = aggregate(_occurredEvents, _newEventName, _mapping);
+      _occurredEvents.clear();
+      return aggregation;
+    } else {
+      return null;
+    }
+  }
+}
+
+/// [EventStream] that can be observed.
+///
+/// Besides all the usual ways of observing a plain [Stream], this event stream
+/// provides more options of observing events, such as aggregating multiple
+/// events into one.
+class ObservableEventStream<T> extends EventStream<T> {
+
+  /// Create observable event stream, based of the specified [controller].
+  ObservableEventStream(StreamController<Event<T>> controller) : super(controller);
+
+  /// Get stream of all events, that occur on this events stream.
+  Stream<Event<T>> get stream => _controller.stream;
+
+  /// Get stream, that produces events that aggregate multiple separate events
+  /// in one.
+  ///
+  /// Create event aggregations using [strategy].
+  ///
+  /// Only aggregated events will be published to the returned stream.
+  Stream<Event<T>> aggregate(AggregationStrategy<T> strategy) {
+    return stream.map(strategy.tryToAggregateOn).where((e) => e != null);
+  }
+}
--- test/event_test.dart
+++ test/event_test.dart
@@ -1,46 +1,170 @@
 import 'dart:async';
 
+import 'package:class_test/class_test.dart';
 import 'package:flutter_event_projections/src/event.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+class EventStreamTest extends Test {
+  StreamController<Event<int>> controller;
+  Stream<Event<int>> stream;
+  EventStream<int> events;
 
-void main() {
-  group('EventStream', () {
-    StreamController<Event<int>> controller;
-    EventStream<int> events;
-    setUp(() {
-      controller = StreamController.broadcast();
-      events = EventStream(controller);
-    });
-    tearDown(() {
-      controller.close();
-    });
-    test('publishes event to underlying stream', () {
+  EventStreamTest({name = 'EventStream'}) : super(name);
+
+  @override
+  void setUp() {
+    controller = StreamController.broadcast();
+    stream = controller.stream;
+    events = EventStream(controller);
+  }
+
+  @override
+  void declareTests() {
+    declareTest('publishes event to underlying stream', () {
       // given
       final expectedEvent = Event('Dummy Event', <String, int>{});
       // when
-      controller.stream.listen(expectAsync1((event) {
+      stream.listen(expectAsync1((event) {
         expect(event, expectedEvent);
       }));
       events.publish(expectedEvent);
     });
-    test('publishes error to underlying stream', () {
+    declareTest('publishes error to underlying stream', () {
       // given
       final expectedException = Exception('Error has happenned');
       // when
-      controller.stream
+      stream
           .handleError(expectAsync1((e) {
               expect(e, expectedException);
           }))
           .listen((e) {});
       events.error(expectedException);
     });
-  });
-  group('Event', () {
-    const name = 'Purchase';
-    const entity = 'user';
-    const expectedId = 15;
-    test('copies map of entity names and their IDs during event creation', () {
+  }
+}
+
+class ObservableEventStreamTest extends EventStreamTest {
+  static final int bread1 = 1, butter1 = 2, sausage1 = 3, bread2 = 4,
+      butter2 = 5, sausage2 = 6;
+  static final String breadIsPlasteredWithButter = 'bread is plastered with butter';
+  static final String sausageIsPlacedOnBread = 'sausage is placed on bread';
+  static final String sandwichIsReady = 'sandwich is ready';
+  final expectedEvents = <Event<int>>[
+    Event(breadIsPlasteredWithButter, {'bread': bread1, 'butter': butter1}),
+    Event(sausageIsPlacedOnBread, {'bread': bread1, 'sausage': sausage1}),
+    Event('some random event', {}),
+    Event(breadIsPlasteredWithButter, {'bread': bread2, 'butter': butter2}),
+    Event('some random event', {}),
+    Event(sausageIsPlacedOnBread, {'bread': bread2, 'sausage': sausage2})
+  ];
+  final expectedAggregations = <Event<int>>[
+    Event(sandwichIsReady, {
+      'breadWithSausage': bread1,
+      'breadPlasteredWithButter': bread1,
+      'butter': butter1,
+      'sausage': sausage1
+    }),
+    Event(sandwichIsReady, {
+      'breadWithSausage': bread2,
+      'breadPlasteredWithButter': bread2,
+      'butter': butter2,
+      'sausage': sausage2
+    })
+  ];
+  final mapping = EntityMapping();
+  ObservableEventStream<int> observableEvents;
+  List<Event<int>> receivedEvents;
+
+  ObservableEventStreamTest(): super(name: 'ObservableEventStreamTest');
+
+  @override
+  void setUp() {
+    super.setUp();
+    observableEvents = ObservableEventStream(controller);
+    events = observableEvents;
+    stream = observableEvents.stream;
+    mapping.set(breadIsPlasteredWithButter, 'bread', 'breadPlasteredWithButter');
+    mapping.set(sausageIsPlacedOnBread, 'bread', 'breadWithSausage');
+    receivedEvents = [];
+  }
+
+  @override
+  void declareTests() {
+    super.declareTests();
+    declareTest('aggregates multiple events into one sequentially', () {
+      // when
+      stream = observableEvents.aggregate(Sequential([breadIsPlasteredWithButter, sausageIsPlacedOnBread], sandwichIsReady, mapping: mapping));
+      stream.listen(expectAsync1((e) {
+        receivedEvents.add(e);
+        if (receivedEvents.length == expectedAggregations.length) {
+          expect(receivedEvents, expectedAggregations);
+        }
+      }, count: expectedAggregations.length));
+      expectedEvents.forEach((e) => observableEvents.publish(e));
+    });
+    declareTest('aggregates mutliple events into one sequentially keeping track of timeout', () {
+      // when
+      stream = observableEvents.aggregate(Sequential([breadIsPlasteredWithButter, sausageIsPlacedOnBread], sandwichIsReady, mapping: mapping, timeout: 500));
+      stream.listen(expectAsync1((e) {
+        receivedEvents.add(e);
+        if (receivedEvents.length == expectedAggregations.length) {
+          expect(receivedEvents, expectedAggregations);
+        }
+      }, count: expectedAggregations.length));
+      expectedEvents.forEach((e) => observableEvents.publish(e));
+    });
+    declareTest('aggregates multiple events without explicit mapping', () {
+      // given
+      final expectedAggregations = [
+        Event(sandwichIsReady, {
+          'bread': bread1,
+          'butter': butter1,
+          'sausage': sausage1
+        }),
+        Event(sandwichIsReady, {
+          'bread': bread2,
+          'butter': butter2,
+          'sausage': sausage2
+        })
+      ];
+      // when
+      stream = observableEvents.aggregate(Sequential([breadIsPlasteredWithButter, sausageIsPlacedOnBread], sandwichIsReady));
+      stream.listen(expectAsync1((e) {
+        receivedEvents.add(e);
+        if (expectedAggregations.length == receivedEvents.length) {
+          expect(receivedEvents, expectedAggregations);
+        }
+      }, count: expectedAggregations.length));
+      expectedEvents.forEach((e) => observableEvents.publish(e));
+    });
+    declareTest('does not aggregate first portion of events due to timeout, but aggregates the second portion', () async {
+      // when
+      stream = observableEvents.aggregate(Sequential([breadIsPlasteredWithButter, sausageIsPlacedOnBread], sandwichIsReady, mapping: mapping, timeout: 500));
+      stream.listen(expectAsync1((event) {
+        expect(event, expectedAggregations[1]);
+      }));
+      for (var i = 0; i < expectedEvents.length; i++) {
+        // Emulate second event missing
+        if (i == 1) {
+          await Future.delayed(Duration(milliseconds: 600));
+          continue;
+        }
+        observableEvents.publish(expectedEvents[i]);
+      }
+    });
+  }
+}
+
+class EventTest extends Test {
+  final name = 'Purchase';
+  final entity = 'user';
+  final expectedId = 15;
+
+  EventTest() : super('Event');
+
+  @override
+  void declareTests() {
+    declareTest('copies map of entity names and their IDs during event creation', () {
       // given
       Map<String, int> entityToId = {'user': 1, 'product': 15, 'item': 32};
       // when
@@ -49,7 +173,7 @@ void main() {
       // then
       expect(event.getIdOf('sale'), isNull);
     });
-    test('returns ID of entity affected by event', () {
+    declareTest('returns ID of entity affected by event', () {
       // given
       final event = Event(name, {entity: expectedId});
       // when
@@ -57,14 +181,14 @@ void main() {
       // then
       expect(id, expectedId);
     });
-    test('two similar events should be equal', () {
+    declareTest('two similar events should be equal', () {
       // when
       final one = Event(name, {entity: expectedId});
       final another = Event(name, {entity: expectedId});
       // then
       expect(one, another);
     });
-    test('returns map of all entities affected by event to their IDs', () {
+    declareTest('returns map of all entities affected by event to their IDs', () {
       // given
       Map<String, int> expectedMap = {'user': 1, 'product': 15, 'item': 32};
       final event = Event(name, expectedMap);
@@ -73,7 +197,7 @@ void main() {
       // then
       expect(map, expectedMap);
     });
-    test('map, returned by the event, should not influence events internal state', () {
+    declareTest('map, returned by the event, should not influence events internal state', () {
       // given
       Map<String, int> expectedMap = {'user': 1, 'product': 15, 'item': 32};
       final event = Event(name, expectedMap);
@@ -84,5 +208,11 @@ void main() {
       expect(event.toMap(), expectedMap);
       expect(event.getIdOf('sale'), isNull);
     });
-  });
+  }
+}
+
+void main() {
+  EventStreamTest().run();
+  ObservableEventStreamTest().run();
+  EventTest().run();
 }
